0cdf783425c35f721eabc907d5160a4c4d1d45f7
myazann <mertyazan2867@gmail.com>
2025-02-20 14:50:32 +0100

    created pruned dataset

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from dataset import get_dataset, get_personas
 
-dataset = get_dataset("test")
+dataset = get_dataset()
+personas = get_personas(dataset, "test")
 
-personas = get_personas(dataset)
+print(personas[:10])
