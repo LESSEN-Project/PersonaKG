885cbeadf4760acad8b21c210ae0fe5f4d2aeebf
myazann <mertyazan2867@gmail.com>
2025-04-28 14:25:46 +0200

    added normalization

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import os
 import hashlib
 import random
+import json
 
 from dataset import get_dataset, get_personas
 from models import LLM
 from knowledge_graph import KnowledgeGraph
 from prompts import *
+from normalization import normalize_attribute
 
 neo4j_password = os.environ.get("NEO4J_PKG_PASSWORD")
 kg = KnowledgeGraph(uri="bolt://localhost:7687", user="neo4j", password=neo4j_password)
@@ -24,7 +26,21 @@ for persona in selected_personas:
     print(f"Processing persona\n{persona}")
     res = persona_kg_extractor.generate(prompt_params={"persona": persona}, json_output=True)
     print(res)
+    
+    res_dict = json.loads(res) if isinstance(res, str) else res
+    
+    array_categories = ['socialConnections', 'personalityTraits', 'interestsAndHobbies', 'skillsAndAbilities', 
+                        'preferencesAndFavorites', 'goalsAndAspirations', 'beliefsAndValues', 'behavioralPatterns']
+    
+    for category in array_categories:
+        if category in res_dict and isinstance(res_dict[category], list):
+            res_dict[category] = [normalize_attribute(attr) for attr in res_dict[category]]
+    
+    if 'additionalAttributes' in res_dict and isinstance(res_dict['additionalAttributes'], list):
+        res_dict['additionalAttributes'] = [normalize_attribute(attr) for attr in res_dict['additionalAttributes']]
+    
     attributes = kg.get_existing_attributes()
-    canonized_res = persona_canonicalizer.generate(prompt_params={"existing_attributes": attributes, "persona_json": res}, json_output=True)
+    normalized_res = json.dumps(res_dict) if not isinstance(res_dict, str) else res_dict
+    canonized_res = persona_canonicalizer.generate(prompt_params={"existing_attributes": attributes, "persona_json": normalized_res}, json_output=True)
     
     kg.upsert_persona(canonized_res, persona_id)
